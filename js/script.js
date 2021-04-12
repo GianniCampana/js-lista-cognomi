7db5ae777b80f8ccb9e8c0c693eda639058aa79b
GianniCampana <campanagianni97@gmail.com>
2021-04-12 17:43:02 +0200

    lista cognomi

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+var lista_cognomi = ["rossi", "verdi", "bianchi", "esposito", "fontana"];
+lista_cognomi.push (prompt("inserisci il tuo cognome"));
+lista_cognomi.sort();
+
+for(i=0; i<6; i++){
+    
+    
+
+}
+
+
+document.getElementById("nomi").innerHTML += '<li>' + lista_cognomi + '<li>';
